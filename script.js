b2a759c8672b06c2ac10091109a1c3919ce287f9
Nirab Shrestha <074msccd011.nirab@pcampus.edu.np>
2024-02-07 13:03:27 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,72 +1,56 @@
-// Initialize the map
 var map = L.map('map').setView([37.0902, -95.7129], 4);
 
-// Add OpenStreetMap tile layer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    subdomains: 'abcd',
-    minZoom: 0,
-    maxZoom: 20,
-    ext: 'png'
+attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+maxZoom: 18,
 }).addTo(map);
 
-// Define a custom icon
-var lightRedIcon = L.icon({
-    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
-    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    shadowSize: [41, 41]
-});
-
-// Initialize marker clustering
 var markers = L.markerClusterGroup();
 
-// Fetching and adding Emergency Operations Centers (EOC) to the marker cluster with the new icon
 fetch('https://services1.arcgis.com/Hp6G80Pky0om7QvQ/arcgis/rest/services/Local_Emergency_Operations_Centers_EOC/FeatureServer/0/query?where=1=1&outFields=*&f=geojson')
-    .then(response => response.json())
-    .then(data => {
-        data.features.forEach(feature => {
-            var latlng = L.latLng(feature.geometry.coordinates[1], feature.geometry.coordinates[0]);
-            var popupContent = "<b>Name:</b> " + feature.properties.NAME + "<br>" +
-                "<b>Address:</b> " + feature.properties.ADDRESS + "<br>" +
-                "<b>Telephone:</b> " + feature.properties.TELEPHONE;
-            var marker = L.marker(latlng, {icon: lightRedIcon}).bindPopup(popupContent);
-            markers.addLayer(marker);
-        });
-        map.addLayer(markers);
+.then(response => response.json())
+.then(data => {
+    data.features.forEach(feature => {
+        var latlng = L.latLng(feature.geometry.coordinates[1], feature.geometry.coordinates[0]);
+        var popupContent = "<b>Name:</b> " + feature.properties.NAME + "<br>" +
+            "<b>Address:</b> " + feature.properties.ADDRESS + "<br>" +
+            "<b>Telephone:</b> " + feature.properties.TELEPHONE;
+
+        // Using L.circleMarker for custom round markers
+        var marker = L.circleMarker(latlng, {
+            radius: 6, // Equivalent to 12pt, depending on your definition. Adjust as needed.
+            fillColor: "#FF6666",
+            color: "#D80000",
+            weight: 1,
+            opacity: 1,
+            fillOpacity: 0.8
+        }).bindPopup(popupContent);
+
+        markers.addLayer(marker);
     });
 
-// Adding Esri feature layer for the Forest Boundary
-var forestLayer = L.esri.featureLayer({
-    url: 'https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/MTBS_Polygons_v1/FeatureServer/0',
-    onEachFeature: function (feature, layer) {
-        var popupContent = "<b>Name:</b> " + feature.properties.FireName + "<br>" +
-            "<b>Fire Type:</b> " + feature.properties.FireType + "<br>" +
-            "<b>Acres burned:</b> " + feature.properties.Acres;
-        layer.bindPopup(popupContent);
-    }
+    map.addLayer(markers);
+});
+
+L.esri.featureLayer({
+url: 'https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/MTBS_Polygons_v1/FeatureServer/0',
+onEachFeature: function (feature, layer) {
+    var popupContent = "<b>Name:</b> " + feature.properties.FireName + "<br>" +
+        "<b>Fire Type:</b> " + feature.properties.FireType + "<br>" +
+        "<b>Acres burned:</b> " + feature.properties.Acres;
+    layer.bindPopup(popupContent);
+}
 }).addTo(map);
 
-// Adding a geoJSON layer for the US State Boundary
 var usstate = L.geoJSON(null, {
-    style: function (feature) {
-        return {
-            fill: false,
-            color: 'black',
-            weight: 2,
-            opacity: 1
-        };
-    },
-    onEachFeature: function (feature, layer) {
-        layer.bindPopup(feature.properties.STATE_NAME);
-    }
+style: function (feature) {
+    return {fill: false, color: 'black', weight: 2, opacity: 1};
+},
+onEachFeature: function (feature, layer) {
+    layer.bindPopup(feature.properties.STATE_NAME);
+}
 }).addTo(map);
 
-// Fetching and adding US State Boundary data
 fetch('https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/USA_Boundaries_2022/FeatureServer/1/query?where=1=1&outFields=*&f=geojson')
-    .then(response => response.json())
-    .then(data => {
-        usstate.addData(data);
-    });
+.then(response => response.json())
+.then(data => usstate.addData(data));
